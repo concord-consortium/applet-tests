bc9d6740b70a88323efdeca60904324de7950ffc
Scott Cytacki <scytacki@concord.org>
2013-06-26 15:49:48 -0400

    add timing test

---
+++ timing/applets/DetectionApplet.java
@@ -0,0 +1,20 @@
+package applets;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class DetectionApplet extends Applet {
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public void paint(Graphics g) {
+		g.clearRect(0, 0, getWidth(), getHeight());
+		
+		g.drawString("Loading...", (getWidth()/2)-29, (getHeight()/2)+7);
+	}
+	
+	public boolean areYouLoaded() {
+		return true;
+	}
+
+}
