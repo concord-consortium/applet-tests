44bf77462521db62efebb9e7cb5530f477612385
Scott Cytacki <scytacki@concord.org>
2013-06-29 22:36:08 -0400

    simplify applet

--- timing/applets/DetectionApplet.java
+++ timing/applets/DetectionApplet.java
@@ -4,16 +4,13 @@ import java.applet.Applet;
 import java.awt.Graphics;
 
 public class DetectionApplet extends Applet {
-	private static final long serialVersionUID = 1L;
+  @Override
+  public void paint(Graphics g) {
+    g.clearRect(0, 0, getWidth(), getHeight());
+    g.drawString("Loading...", 10, 10);
+  }
 
-	@Override
-	public void paint(Graphics g) {
-		g.clearRect(0, 0, getWidth(), getHeight());
-		g.drawString("Loading...", (getWidth()/2)-29, (getHeight()/2)+7);
-	}
-
-	public boolean areYouLoaded() {
-		return true;
-	}
-
+  public boolean areYouLoaded() {
+    return true;
+  }
 }
