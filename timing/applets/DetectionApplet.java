f25eb1f0352cc7b236be1878193b61b58319ec29
Scott Cytacki <scytacki@concord.org>
2013-06-29 22:13:10 -0400

    clean up some whitespace

--- timing/applets/DetectionApplet.java
+++ timing/applets/DetectionApplet.java
@@ -5,14 +5,13 @@ import java.awt.Graphics;
 
 public class DetectionApplet extends Applet {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Override
 	public void paint(Graphics g) {
 		g.clearRect(0, 0, getWidth(), getHeight());
-		
 		g.drawString("Loading...", (getWidth()/2)-29, (getHeight()/2)+7);
 	}
-	
+
 	public boolean areYouLoaded() {
 		return true;
 	}
