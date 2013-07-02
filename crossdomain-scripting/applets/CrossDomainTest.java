0d96fcfd90d9136c6bd554b5a326fc007393cea0
Scott Cytacki <scytacki@concord.org>
2013-07-02 17:05:27 -0400

    expose the code of the test better

--- crossdomain-scripting/applets/CrossDomainTest.java
+++ crossdomain-scripting/applets/CrossDomainTest.java
@@ -6,33 +6,28 @@ import java.awt.Graphics;
 import netscape.javascript.JSObject;
 
 public class CrossDomainTest extends Applet {
-	private static final long serialVersionUID = 1L;
+  @Override
+  public void paint(Graphics g) {
+    g.drawString("CrossDomain Test", 10, 20);
+  }
 
-    @Override
-	public void paint(Graphics g) {
-		g.drawString("CrossDomain Test", 5, (getHeight()/2)+7);
-	}
+  public boolean instanceMethod(){
+    return true;
+  }
 
-	public boolean instanceMethod(){
-		return true;
-	}
-
-	public void roundTrip(String sameThreadCodeToEval, final String differenThreadCodeToEval) {
-		JSObject window = JSObject.getWindow(this);
-		window.eval(sameThreadCodeToEval);
-		try{
-			(new Thread(){
-				public void run(){
-					try {
-						JSObject window = JSObject.getWindow(CrossDomainTest.this);
-						window.eval(differenThreadCodeToEval);
-					} catch (Throwable t) {
-						t.printStackTrace();
-					}
-				}
-			}).start();
-		} catch (Throwable t) {
-			t.printStackTrace();
-		}
-	}
+  public void roundTrip(
+  	  String sameThreadCodeToEval,
+  	  final String differenThreadCodeToEval) {
+    final JSObject window = JSObject.getWindow(this);
+    window.eval(sameThreadCodeToEval);
+    try{
+      (new Thread(){
+        public void run(){
+          window.eval(differenThreadCodeToEval);
+        }
+      }).start();
+    } catch (Throwable t) {
+      t.printStackTrace();
+    }
+  }
 }
---
+++ crossdomain-scripting/index.md
@@ -0,0 +1,26 @@
+---
+layout: default
+title: Cross domain scripting
+show_in_toc: true
+---
+
+There were limitations on where an applet can come from and whether scripts can access it from the page. This used to be a problem in Safari but so far
+I haven't been able to reproduce it. Currently this test loads 2 signed applets one from the same host and one from a different host.
+It then calls methods on each one and also has each one call javascript.
+[A discussion about scripting cross domain on safari](http://jogamp.org/applet-launcher/www/#SCRIPTING)
+
+The jar of the applets is also marked as a trusted-library. Otherwise a message is shown to the user when javascript tries to access
+a signed applet. [Offical docs about Trusted-Library and Javascript](http://docs.oracle.com/javase/7/docs/technotes/guides/jweb/mixed_code.html)
+
+[Run Test](test.html)
+
+<div>
+<div id='gist-it-xdomainjs' style='width: 34em; float: left; margin-left: 10px'>
+<script src="http://gist-it.appspot.com/github/{{ site.github_repo }}/blob/gh-pages/crossdomain-scripting/test.html?slice=28:51">
+</script>
+</div>
+<div id='gist-it-appletjs' style='width: 24em; float: left; margin-left: 10px'>
+<script src="http://gist-it.appspot.com/github/{{ site.github_repo }}/blob/gh-pages/crossdomain-scripting/applets/CrossDomainTest.java?slice=7:">
+</script>
+</div>
+</div>
